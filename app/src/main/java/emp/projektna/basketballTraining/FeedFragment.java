cc1703b05d2f031f347c380d25583d7015431c83
Andraž Anderle <andraz.anderle@gmail.com>
2020-10-27 14:32:53 +0100

    UI newsFeed kontejnerja

--- app/src/main/java/emp/projektna/basketballTraining/FeedFragment.java
+++ app/src/main/java/emp/projektna/basketballTraining/FeedFragment.java
@@ -4,16 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.lang.reflect.Array;
 
 public class FeedFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+
+        View view = inflater.inflate(R.layout.row_feed, container, false);
+    /*
+        ListView listView = view.findViewById(R.id.lisViewFeed);
+
+        String[] menuItems = {"blabla", "blabla2", "blabla3"};
+
+        ArrayAdapter listViewAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, menuItems);
+
+        listView.setAdapter(listViewAdapter);
+*/
+        return view;
     }
 }
